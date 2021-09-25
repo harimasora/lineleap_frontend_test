7ac2dbaaf57c8946e39bab2bc3cc945208e6b999
Danilo Maia Rodrigues <zidalo@gmail.com>
2021-09-24 23:40:14 -0300

    feat: Set Lato as default font family

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
+        fontFamily: 'Lato',
         primarySwatch: Colors.blue,
       ),
       home: HomePage(),
